2e6b6e5416b34719a34d833d6c2936715ec0c920
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-10-18 23:36:22 +0600

    jwt secret

---
+++ controllers/user.controller.ts
@@ -0,0 +1,56 @@
+import { Request, Response, NextFunction } from "express";
+import userModel, { IUser } from "../models/user.model";
+import ErrorHandler from "../utils/ErrorHandler";
+import { CatchAsyncError } from "../middleware/catchAsyncError";
+import jwt, { Secret } from "jsonwebtoken";
+require("dotenv").config();
+
+// register user
+interface IRegistrationBody {
+  name: string;
+  email: string;
+  password: string;
+  avatar?: string;
+}
+
+export const registrationUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { name, email, password } = req.body;
+      const isEmailExist = await userModel.findOne({ email });
+      if (isEmailExist) {
+        return next(new ErrorHandler("Email already exist", 400));
+      }
+      const user: IRegistrationBody = {
+        name,
+        email,
+        password,
+      };
+      const activationToken = createActivationToken(user);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+// activation interface
+interface IActivationToken {
+  token: string;
+  activationCode: string;
+}
+
+export const createActivationToken = (user: IUser): IActivationToken => {
+  const activationCode = Math.floor(1000 + Math.random() * 9000).toString();
+
+  const token = jwt.sign(
+    { user, activationCode },
+    process.env.ACTIVATION_SECRET as Secret,
+    {
+      expiresIn: "5m",
+    }
+  );
+  return {
+    token,
+    activationCode,
+  };
+};
