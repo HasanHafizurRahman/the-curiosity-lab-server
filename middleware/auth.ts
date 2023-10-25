afbb9d55c4090c22efe7f7777913248dd704795b
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-10-26 00:24:01 +0600

    global types declare

---
+++ middleware/auth.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from "express";
+import { CatchAsyncError } from "./catchAsyncError";
+import ErrorHandler from "../utils/ErrorHandler";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { redis } from "../utils/redis";
+
+export const isAuthenticated = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const access_token = req.cookies.access_token;
+    if (!access_token) {
+      return next(
+        new ErrorHandler("Please login to access this resource", 400)
+      );
+    }
+
+    const decoded = jwt.verify(
+      access_token,
+      process.env.ACCESS_TOKEN as string
+    ) as JwtPayload;
+    if (!decoded) {
+      return next(new ErrorHandler("Access token is not valid", 400));
+    }
+
+    const user = await redis.get(decoded.id);
+    if (!user) {
+      return next(new ErrorHandler("User not found", 400));
+    }
+
+    req.user = JSON.parse(user);
+    next();
+  }
+);
